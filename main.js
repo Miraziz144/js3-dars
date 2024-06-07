323218fdc870ea9e724870f77a3b512a3d68e15f
Miraziz144 <nurullayevmiraziz4@gmail.com>
2024-06-07 09:58:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+//alert('hozir sizga random son chiqishni boshlaydi')
+alert(Math.floor(Math.random(99) * 200))
+
+alert(Math.random() %  2)
+
+if (Math.floor)
+
+console.log(Math.floor(Math.random(99) * 200));
